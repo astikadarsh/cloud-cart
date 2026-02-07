5dbc6cd5486f34a957dcc32fed5efc94abc35529
astikadarsh <astikadarsh7061@gmail.com>
2026-02-07 15:32:07 +0530

    initial commit

---
+++ cloudcart/src/app/page.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar"
+
+
+export default function Home() {
+  return (
+
+    <div className="container bg-purple-500">
+      <Navbar />
+      <p className="text-white">Officially started major project</p>
+    </div>
+  );
+}
